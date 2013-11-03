4e6ed200f6ad3a176518a2721905c879d981744a
Ben McEldowney <ben@ratiointeractive.com>
2013-11-03 09:18:54 -0800

    reworking page integration

--- WindowsGridApp/js/default.js
+++ WindowsGridApp/js/default.js
@@ -1,10 +1,6 @@
-﻿angular.module('app', ['ngRoute']);
+﻿angular.module('app', ['ratioListView']);
 
-angular.module('app').config(function () {
-
-});
-
-angular.module('app').run(function () {
+angular.module('app').run(function ($compile, $rootScope, $timeout) {
     "use strict";
 
     WinJS.Binding.optimizeBindingReferences = true;
@@ -35,6 +31,15 @@ angular.module('app').run(function () {
                 }
             }));
         }
+
+        //Application.navigator.addEventListener('pageLoaded', function (arg) {
+        //    args = arg.detail;
+        $rootScope.$on('ready', function (evt, args) {
+            $timeout(function () {
+                $rootScope.navOptions = args.options || {};
+                $compile(args.element)($rootScope);
+            })
+        });
     });
 
     app.oncheckpoint = function (args) {
@@ -46,4 +51,74 @@ angular.module('app').run(function () {
     };
 
     app.start();
+});
+
+angular.module('app').controller('groupDetailCtrl', function ($scope, $timeout) {
+    var appView = Windows.UI.ViewManagement.ApplicationView;
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    var nav = WinJS.Navigation;
+    var options = $scope.navOptions;
+    var group = (options && options.groupKey) ? Data.resolveGroupReference(options.groupKey) : Data.groups.getAt(0);
+    var items = Data.getItemsFromGroup(group);
+    var pageList = items.createGrouped(
+        function groupKeySelector(item) { return group.key; },
+        function groupDataSelector(item) { return group; }
+    );
+
+    var navigateToGroup = function (key) {
+        nav.navigate("/pages/groupDetail/groupDetail.html", { groupKey: key });
+    }
+    var initializeLayout = function (viewState) {
+        var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+        if (viewState === appViewState.snapped) {
+            $scope.layout = new WinJS.UI.ListLayout();
+        } else {
+            $scope.layout = new WinJS.UI.GridLayout({ groupHeaderPosition: "left" });
+        }
+    }
+    var itemInvoked = function (args) {
+        if (appView.value === appViewState.snapped) {
+            // If the page is snapped, the user invoked a group.
+            var group = Data.groups.getAt(args.detail.itemIndex);
+            navigateToGroup(group.key);
+        } else {
+            // If the page is not snapped, the user invoked an item.
+            var item = Data.items.getAt(args.detail.itemIndex);
+            nav.navigate("/pages/itemDetail/itemDetail.html", { item: Data.getItemReference(item) });
+        }
+    }
+
+    $scope.itemInvoked = itemInvoked;
+    $scope.animateLayout = true;
+    $scope.title = group.title;
+    $scope.itemDataSource = pageList.dataSource;
+    $scope.groupDataSource = pageList.groups.dataSource;
+    initializeLayout(Windows.UI.ViewManagement.ApplicationView.value);
+
+    //the parent controller on a page needs to destroy itself on navigation
+    //this is needed because we are using WinJS navigation and not AngularJS routing
+    $scope.$on('unload', function () {
+        $scope.itemInvoked = null;
+        $scope.title = null;
+        $scope.layout = null;
+        $scope.itemDataSource = null;
+        $scope.groupDataSource = null;
+
+        //destroy on the next turn, else $broadcast will crash trying to communicate with any 
+        //destroyed child scopes
+        $timeout(function () {
+            $scope.$destroy();
+        });
+    });
+
+    $scope.$on('updateLayout', function (evt, args) {
+        if (args.lastViewState !== args.viewState) {
+            $scope.animateLayout = (args.lastViewState === appViewState.snapped || args.viewState === appViewState.snapped)
+
+            $timeout(function () {
+                initializeLayout(args.viewState);
+                $scope.animateLayout = true;
+            });
+        }
+    });
 });
---
+++ WindowsGridApp/modules/ListView.js
@@ -0,0 +1,74 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('ratioListView', []);
+    var isolatedScope = {
+        layout: '=layout',
+        itemDataSource: '=itemDataSource',
+        groupDataSource: '=groupDataSource',
+        itemTemplateId: '@itemTemplateId',
+        headerTemplateId: '@headerTemplateId',
+        animateLayout: '=animateLayout',
+        itemInvoked: '&itemInvoked'
+    };
+
+    angular.module('ratioListView').directive('listView', function ($timeout) {
+        return {
+            scope: isolatedScope,
+            link: function (scope, element, attr) {
+                var listView = new WinJS.UI.ListView(element[0], { selectionMode: 'none' });
+
+                listView.itemTemplate = document.getElementById(scope.itemTemplateId);
+                listView.groupHeaderTemplate = document.getElementById(scope.headerTemplateId);
+
+                scope.$watch('itemDataSource', function () {
+                    listView.itemDataSource = scope.itemDataSource;
+                });
+
+                scope.$watch('groupDataSource', function () {
+                    listView.groupDataSource = scope.groupDataSource;
+                });
+
+                scope.$watch('itemInvoked', function () {
+                    listView.oniteminvoked = function (args) {
+                        scope.itemInvoked({ args: args });
+                    }
+                })
+
+                scope.$watch('layout', function () {
+                    var handler = function (e) {
+                        listView.removeEventListener("contentanimating", handler, false);
+                        e.preventDefault();
+                    }
+
+                    //this may or may not be something that is desired universally for ListViews
+                    if (scope.animateLayout) {
+                        listView.addEventListener("contentanimating", handler, false);
+                    }
+
+                    listView.layout = scope.layout;
+                    var firstVisible = listView.indexOfFirstVisible;
+                    if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                        listView.indexOfFirstVisible = firstVisible;
+                    }
+                });
+
+                //hacky timeout to wait to focus until the page loading animation finishes
+                //could be replaced if an event exits that fires when animation is finished
+                $timeout(function () {
+                    listView.element.focus();
+                }, 750);
+
+                scope.$on('$destroy', function () {
+                    scope.itemDataSource = null;
+                    scope.groupDataSource = null;
+                    scope.layout = null;
+                    scope.itemInvoked = null;
+                    listView = null;
+
+                    element.remove();
+                })
+            }
+        };
+    });
+})()
